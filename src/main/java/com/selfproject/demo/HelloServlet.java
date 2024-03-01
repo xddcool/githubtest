90301c4fec93b0be605f8d76519637cf1fb6a96a
Daniel Deng <danielxdeng@gmail.com>
2024-03-01 13:18:05 -0500

    init

--- src/main/java/com/selfproject/demo/HelloServlet.java
+++ src/main/java/com/selfproject/demo/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
 
     public void init() {
         message = "Hello World!";
-    }//test
+    }//test test2
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
